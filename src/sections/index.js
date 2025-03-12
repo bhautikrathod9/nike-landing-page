39d057ceb6cdb04b623e7dfeb98befc8e082b90b
gogo <hitmangogo7@gmail.com>
2025-03-12 11:53:06 +0530

    made navbar

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from './Footer.jsx';
+import Services from "./Services.jsx";
+import PopularProducts from './PopularProducts.jsx';
+import SuperQuality from './SuperQuality.jsx';
+import SpecialOffer from './SpecialOffer.jsx';
+import Subscribe from './Subscribe.jsx';
+
+
+export {
+    Hero, 
+    CustomerReviews,
+    Footer,
+    Services,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe
+}
