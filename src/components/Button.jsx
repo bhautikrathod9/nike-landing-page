9db84ceeb5d5482b9969d9ac48d25fd331fc5b31
gogo <hitmangogo7@gmail.com>
2025-03-13 14:04:18 +0530

    add hover effects on components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({label, iconURL}) => {
   return ( 
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red text-white border-coral-red rounded-full hover:bg-red-500 cursor-pointer'>
+    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red text-white border-coral-red rounded-full hover:bg-red-500 cursor-pointer hover:scale-105 hover:shadow-xl cursor-pointer transition-transform transform duration-300'>
         {label}
         <img 
             src={iconURL}
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,12 +1,13 @@
 import { arrowRight } from "../assets/icons";
 import { shoe8 } from "../assets/images";
-import Button from '../components/Button'
+import Button from '../components/Button';
+import {useState, useRef, useEffect } from 'react';
 
 const SuperQuality = () => {
   return (
     <section
       id='about-us'
-      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
+      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container relative group transition-transform duration-300 ease-in-out overflow-hidden'
     >
       <div className='flex flex-1 flex-col'>
         <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
@@ -27,13 +28,13 @@ const SuperQuality = () => {
         </div>
       </div>
 
-      <div className='flex-1 flex justify-center items-center'>
+      <div className='flex-1 flex justify-center items-center transition-transform duration-300 ease-in-out transform scale-100 group-hover:scale-105 py-10'>
         <img
           src={shoe8}
           alt='product detail'
           width={570}
           height={522}
-          className='object-contain'
+          className='object-contain '
         />
       </div>
     </section>
